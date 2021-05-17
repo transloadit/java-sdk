d30ded4f26fb28d5d7114d056e7e81fe6ffcfc04
florian <60937022+cdr-chakotay@users.noreply.github.com>
2021-05-17 20:02:02 +0200

    - finished Linting of exceptions package - added package-info.java

--- src/main/java/com/transloadit/sdk/exceptions/RequestException.java
+++ src/main/java/com/transloadit/sdk/exceptions/RequestException.java
@@ -4,10 +4,18 @@ package com.transloadit.sdk.exceptions;
  * Exception thrown whenever something goes wrong while communicating with Transloadit API.
  */
 public class RequestException extends Exception {
+    /**
+     * Wraps an exception in a RequestException.
+     * @param e {@link Exception}
+     */
     public RequestException(Exception e) {
         super(e);
     }
 
+    /**
+     * Constructs a new RequestException with the specified detail message.
+     * @param msg Detail message
+     */
     public RequestException(String msg) {
         super(msg);
     }
