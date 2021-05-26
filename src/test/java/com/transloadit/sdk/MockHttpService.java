056a67d1f8fecd208f615e5e8caf3e19c23793ff
florian <60937022+cdr-chakotay@users.noreply.github.com>
2021-05-26 16:53:13 +0200

    - finished MockHttpService.java

--- src/test/java/com/transloadit/sdk/MockHttpService.java
+++ src/test/java/com/transloadit/sdk/MockHttpService.java
@@ -4,12 +4,22 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-
+/**
+ * This class provides an universal Template for running mock-http Test with identical settings.
+ */
 public class MockHttpService {
+    //CHECKSTYLE:OFF
     protected final int PORT = 9040;
     protected final Transloadit transloadit = new Transloadit("KEY", "SECRET", "http://localhost:" + PORT);
+    //CHECKSTYLE:ON
 
-    protected String getJson (String name) throws IOException {
+    /**
+     * Loads test resources from src/test/resources/__files/ with the specified name and provides them as a JSON String.
+     * @param name name of test resource.
+     * @return JSON string of test resource
+     * @throws IOException if specified test resource can't be found.
+     */
+    protected String getJson(String name) throws IOException {
         String filePath = "src/test/resources/__files/" + name;
 
         BufferedReader br = new BufferedReader(new FileReader(filePath));
