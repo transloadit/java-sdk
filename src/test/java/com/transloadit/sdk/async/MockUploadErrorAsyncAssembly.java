3a22fd8564ba151d7f113357e5f49e3607c4809f
florian <60937022+cdr-chakotay@users.noreply.github.com>
2021-06-03 17:04:57 +0200

    - Almost done except AsyncAssemblyTest - TODO: Activate Linter for all files, not only new

--- src/test/java/com/transloadit/sdk/async/MockUploadErrorAsyncAssembly.java
+++ src/test/java/com/transloadit/sdk/async/MockUploadErrorAsyncAssembly.java
@@ -10,18 +10,44 @@ import org.mockito.Mockito;
 
 import java.io.IOException;
 
+/**
+ * This class Mocks an {@link AsyncAssembly}, which has an error during upload.
+ */
 public class MockUploadErrorAsyncAssembly extends AsyncAssembly {
+    /**
+     * Instantiates an {@link AsyncAssembly} object which always throws an error if a file upload attempt is undertaken.
+     * @param transloadit The {@link Transloadit} client.
+     * @param listener An {@link UploadProgressListener}
+     */
     public MockUploadErrorAsyncAssembly(Transloadit transloadit, UploadProgressListener listener) {
         super(transloadit, listener);
         tusClient = new MockTusClient();
     }
 
+    /**
+     * Instantiates an {@link AsyncAssembly} object which always throws an error if a file upload attempt is undertaken.
+     * @param transloadit The {@link Transloadit} client.
+     * @param listener An {@link AssemblyProgressListener}
+     */
     public MockUploadErrorAsyncAssembly(Transloadit transloadit, AssemblyProgressListener listener) {
         super(transloadit, listener);
         tusClient = new MockTusClient();
     }
 
+    /**
+     * Nested class which provides a mocked {@link TusUploader}, which always throws an exception if
+     * {@link TusUploader#uploadChunk()} gets called.
+     */
     class MockTusClient extends TusClient {
+        /**
+         * Instantiates a a mocked {@link TusUploader}, which always throws an exception if
+         * {@link TusUploader#uploadChunk()} gets called.
+         * @param upload {@link TusUpload}
+         * @return {@link TusUploader}, which always throws an exception if {@link TusUploader#uploadChunk()} gets
+         * called.
+         * @throws ProtocolException if {@link TusUploader#uploadChunk()} gets called.
+         * @throws IOException if an IOError occurs.
+         */
         @Override
         public TusUploader resumeOrCreateUpload(@NotNull TusUpload upload) throws ProtocolException, IOException {
             TusUploader uploader = Mockito.mock(TusUploader.class);
