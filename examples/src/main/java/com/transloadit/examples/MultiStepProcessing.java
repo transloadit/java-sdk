d8f12b4a5dfad04abefebc59c98053b2794b9b86
florian <60937022+cdr-chakotay@users.noreply.github.com>
2021-11-24 21:52:57 +0100

    Updated Examples

--- examples/src/main/java/com/transloadit/examples/MultiStepProcessing.java
+++ examples/src/main/java/com/transloadit/examples/MultiStepProcessing.java
@@ -10,7 +10,6 @@ import com.transloadit.sdk.response.AssemblyResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,23 +28,19 @@ public final class MultiStepProcessing {
      * @param args
      */
     public static void main(String[] args) throws FileNotFoundException {
-
         // New Transloadit Instance
         Transloadit transloadit = new Transloadit("TRANSLOADIT_KEY", "TRANSLOADIT_SECRET");
         Assembly assembly = transloadit.newAssembly();
-
         // Add Files and define Field name
-        assembly.addFile(new File(Objects.requireNonNull(MultiStepProcessing.class.getResource("/dutch-anthem.mp3")).getFile()), "file_1");
-        assembly.addFile(new File(Objects.requireNonNull(MultiStepProcessing.class.getResource("/german-anthem-0.mp3")).getFile()), "file_2");
-
+        assembly.addFile(new File(Objects.requireNonNull(MultiStepProcessing.class.getResource(
+                "/dutch-anthem.mp3")).getFile()), "file_1");
+        assembly.addFile(new File(Objects.requireNonNull(MultiStepProcessing.class.getResource(
+                "/german-anthem-0.mp3")).getFile()), "file_2");
         // Step1 Reduce File's Bitrates
         Map<String, Object> step1 = new HashMap<>();
         step1.put("preset", "opus");
         step1.put("bitrate", 128000);
-
         assembly.addStep("encode", "/audio/encode", step1);
-
-
         // Step2 Concatenation
             /* Building "use" parameter as JSONObject
                 -   name = Name of previous step
@@ -53,7 +48,6 @@ public final class MultiStepProcessing {
                 -   as = audio_<number> defines oder of Concatenation
                 => Needs to be stored under key "steps" as it defines every substep
              */
-
         JSONObject outerJsonObject = new JSONObject();
         outerJsonObject.append(
                 "steps",
@@ -72,27 +66,20 @@ public final class MultiStepProcessing {
         Map<String, Object> step2 = new HashMap<>();
         step2.put("preset", "mp3");
         step2.put("use", outerJsonObject);
-
         assembly.addStep("concat", "/audio/concat", step2);
-
-
         // Step 3 Waveform
         Map<String, Object> step3 = new HashMap<>();
         step3.put("use", "concat");
         step3.put("width", 1920);
         step3.put("height", 720);
         step3.put("outer_color", "ff00c7ff");
-
         assembly.addStep("waveform", "/audio/waveform", step3);
-
-
         // Register AssemblyListener => Informs User on Assembly completion
         assembly.setAssemblyListener(new AssemblyListener() {
             @Override
             public void onAssemblyFinished(AssemblyResponse response) {
                 System.out.println("Assembly finished");
             }
-
             public void printStatus(JSONArray status) {
                 for (int i = 0; i < status.length(); i++) {
                     JSONObject obj = status.getJSONObject(i);
@@ -130,12 +117,7 @@ public final class MultiStepProcessing {
                         + result.get("ext"));
                 System.out.println("Downlaodlink: " + result.getString("ssl_url") + "\n");
             }
-
-
         });
-
-
-
         try {
             System.out.println("Processing... ");
             UploadProgressListener uploadProgressListener = new UploadProgressListener() {
@@ -157,8 +139,8 @@ public final class MultiStepProcessing {
                 }
 
                 @Override
-                public void onParallelUploadsStarting(int ParallelUploads, int uploadNumber) {
-                    System.out.println("New Uploads: Max:" + ParallelUploads + " Number in Queue: " + uploadNumber);
+                public void onParallelUploadsStarting(int parallelUploads, int uploadNumber) {
+                    System.out.println("New Uploads: Max:" + parallelUploads + " Number in Queue: " + uploadNumber);
                 }
 
                 @Override
@@ -177,17 +159,16 @@ public final class MultiStepProcessing {
             assembly.setMaxParallelUploads(2); // todo: backward Compability
             assembly.setUploadChunkSize(50);
             assembly.save(true);
-            /**
-            Thread.sleep(50);
+            /*
+            Thread.sleep(50);  // todo: adjust for new modules
             assembly.pauseUploads();
             Thread.sleep(3000);
             assembly.resumeUploads();
-            **/
-
-         //   assembly.pauseUploads();
+            */
+            // assembly.pauseUploads();
             // assembly.abortUploads();
            // assembly.resumeUploads();
-            System.out.println("Assembly ID: " + assembly.getAssemblyID());
+            System.out.println("Assembly ID: " + assembly.getClientSideGeneratedAssemblyID());
             assembly.save(true);
          } catch (LocalOperationException | RequestException e) {
             e.printStackTrace();
