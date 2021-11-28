0b622cfa77554356bbb2292871b6af6318707ac5
florian <60937022+cdr-chakotay@users.noreply.github.com>
2021-11-28 13:24:41 +0100

    Get back to single class imports

--- src/main/java/com/transloadit/sdk/Assembly.java
+++ src/main/java/com/transloadit/sdk/Assembly.java
@@ -16,7 +16,10 @@ import io.tus.java.client.TusUpload;
 import org.jetbrains.annotations.TestOnly;
 import org.json.JSONObject;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
