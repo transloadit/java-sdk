bb8562ea0fe0b0cd1ac90c826520aa2285f720c0
florian <60937022+cdr-chakotay@users.noreply.github.com>
2022-01-12 17:51:01 +0100

    Update Linter to more recent version in order to solve its issues

--- src/main/java/com/transloadit/sdk/Request.java
+++ src/main/java/com/transloadit/sdk/Request.java
@@ -28,7 +28,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Random;
 
 /**
